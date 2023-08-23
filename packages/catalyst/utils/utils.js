eac7b7b3f917b180651ae3bd5a0b95c4a4b83611
Tirtha Guha <tirtha.guha@gmail.com>
2023-08-23 11:33:19 +0530

    Releases (#21)
    * 3 jstbcatalyst feature request component path customization (#8)
    
    * TG: Feature Request #3, component path is now customisable
    
    * new version published
    
    * Fix #10, made all css imports as standard css imports
    
    * Fix #11: path is now in lowercase or kebab-case
    
    * updated version for catalyst
    
    * Added Documentation
    
    * Updated Documentation
    
    * TG: Updated Issue Template
    
    * updated issue templates
    
    * Bugfix (#18)
    
    * Fix #11: path is now in lowercase or kebab-case (#13)
    
    * Releases (#17)
    
    * 3 jstbcatalyst feature request component path customization (#8)
    
    * TG: Feature Request #3, component path is now customisable
    
    * new version published
    
    * Fix #10, made all css imports as standard css imports
    
    * Fix #11: path is now in lowercase or kebab-case
    
    * updated version for catalyst
    
    * Added Documentation
    
    * Updated Documentation
    
    * TG: Updated Issue Template
    
    * updated issue templates
    
    * fix #15
    
    * Fix #14
    
    * FIX #14
    
    * Changed directory structure
    
    * updated versions
    
    * Refactor for containers (#20)
    
    * Releases (#19)
    
    * 3 jstbcatalyst feature request component path customization (#8)
    
    * TG: Feature Request #3, component path is now customisable
    
    * new version published
    
    * Fix #10, made all css imports as standard css imports
    
    * Fix #11: path is now in lowercase or kebab-case
    
    * updated version for catalyst
    
    * Added Documentation
    
    * Updated Documentation
    
    * TG: Updated Issue Template
    
    * updated issue templates
    
    * Bugfix (#18)
    
    * Fix #11: path is now in lowercase or kebab-case (#13)
    
    * Releases (#17)
    
    * 3 jstbcatalyst feature request component path customization (#8)
    
    * TG: Feature Request #3, component path is now customisable
    
    * new version published
    
    * Fix #10, made all css imports as standard css imports
    
    * Fix #11: path is now in lowercase or kebab-case
    
    * updated version for catalyst
    
    * Added Documentation
    
    * Updated Documentation
    
    * TG: Updated Issue Template
    
    * updated issue templates
    
    * fix #15
    
    * Fix #14
    
    * FIX #14
    
    * Changed directory structure
    
    * Refactored directory structure to facilitate other generators
    
    * components directory changed
    
    * Beta version for refactoring

---
+++ packages/catalyst/utils/utils.js
@@ -0,0 +1,31 @@
+import path from "node:path";
+import packageJSONConfig from "@jstb/package-json-reader";
+
+const catalystConfig = packageJSONConfig("catalystConfig");
+
+const defaultConfig = {
+  lang: "js",
+  arch: "atomic",
+  skipStories: false,
+  storyFormat: "script", //TODO
+  styling: "css", //TODO
+  targetBasePath: "/src",
+};
+
+if (catalystConfig) {
+  for (const key in catalystConfig) {
+    if (Object.hasOwnProperty.call(catalystConfig, key)) {
+      defaultConfig[key] = catalystConfig[key];
+    }
+  }
+}
+
+const getDefaultConfig = () => {
+  return defaultConfig;
+};
+
+const getRootDir = () => {
+  return path.resolve(process.cwd());
+};
+
+export { getRootDir, getDefaultConfig };
